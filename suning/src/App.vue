19969425f54ed3dcd8ea7aea51f0bfd89887a056
babafengge <635312963@qq.com>
2020-08-05 09:52:08 +0800

    一些整体样式

--- suning/src/App.vue
+++ suning/src/App.vue
@@ -5,7 +5,17 @@
 </template>
 
 <style>
-
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html,body,#app{
+  height: 100%;
+  overflow: hidden;
+  background-color: #f2f2f2;
+  color: #222;
+}
 </style>
 
 
