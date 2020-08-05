4b62082ac02a7903ee1d3813a7cde31dee93a70e
babafengge <635312963@qq.com>
2020-08-05 09:46:14 +0800

    基本框架结构

---
+++ suning/src/components/TabBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        我是底部
+    </div>
+</template>
+
--- suning/src/router/index.js
+++ suning/src/router/index.js
@@ -1,23 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Classify from '../views/Classify.vue'
+import Detail from '../views/Detail.vue'
 import Home from '../views/Home.vue'
+import My from '../views/My.vue'
+import Search from '../views/Search.vue'
+import ShopCar from '../views/ShopCar.vue'
+
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path:'/classify',
+    component:Classify
+  },
+  {
+    path:'/detail',
+    component:Detail
+  },
+  {
+    path:'/my',
+    component:My
+  },
+  {
+    path:'/search',
+    component:Search
+  },
+  {
+    path:'/shopCar',
+    component:ShopCar
+  },
 ]
 
 const router = new VueRouter({
